54039da811d7823f819a9fb72769b5acac54396f
Mayno971 <noahn.de-lacroix@universite-paris-saclay.fr>
2026-04-11 19:24:23 +0200

    supression dans l'app de rotation

--- app/src/main/java/com/example/applicationmobile/CustomActivity.java
+++ app/src/main/java/com/example/applicationmobile/CustomActivity.java
@@ -1,33 +1,14 @@
 package com.example.applicationmobile;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
 
 public class CustomActivity extends AppCompatActivity {
-    private String classeChoisie = "";
+    private String classeChoisie = "GUERRIER";
     private String pseudoJoueur = "";
-    private String couleurChoisie = "E74C3C";
-
-    private String [] listeChapeaux = {
-            "Couronne en or",
-            "Casque Viking",
-            "Chapeau Sorcier",
-            "Casque Audio",
-            "Auréole Fluo"
-    };
-    private int indexChapeau = 0;
-
-    private ImageView imagePreview;
-    private TextView textCurrentHat;
-    private MaterialButton[] colorButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,18 +29,11 @@ public class CustomActivity extends AppCompatActivity {
                 pseudoJoueur = "Joueur inconnu";
             }
 
+            // On va vers le Lobby (Tutoriel), pas directement à la manette !
             Intent intent = new Intent(CustomActivity.this, LobbyActivity.class);
             intent.putExtra("CLASSE_CHOISIE", classeChoisie);
             intent.putExtra("PSEUDO_JOUEUR", pseudoJoueur);
-            intent.putExtra("COULEUR_JOUEUR", couleurChoisie);
-            intent.putExtra("CHAPEAU_JOUEUR", listeChapeaux[indexChapeau]);
-
             startActivity(intent);
         });
     }
-
-
-
-
-
 }
